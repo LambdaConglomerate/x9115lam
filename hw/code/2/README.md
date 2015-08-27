4af3203ff3188b10c3f68c486d0c93797c1a6193
Alexander Sobran <aisobran@ncsu.edu>
2015-08-27 07:57:48 -0400

    Exercise 3.2

--- hw/code/2/README.md
+++ hw/code/2/README.md
@@ -1,2 +1,6 @@
 3.1:
 ![3.1](./pics/3.1.png)
+
+3.2:
+The program runs as normal.
+![3.2](./pics/3.2.png)
--- hw/code/2/think3.py
+++ hw/code/2/think3.py
@@ -1,10 +1,20 @@
 # 3.1
-repeat_lyrics()
-def print_lyrics():
-    print "I'm a lumberjack, and I'm okay."
-    print "I sleep all night and I work all day."
+# repeat_lyrics()
+# def print_lyrics():
+#     print "I'm a lumberjack, and I'm okay."
+#     print "I sleep all night and I work all day."
 
+# def repeat_lyrics():
+#     print_lyrics()
+#     print_lyrics()
+
+# 3.2
 def repeat_lyrics():
     print_lyrics()
     print_lyrics()
 
+def print_lyrics():
+    print "I'm a lumberjack, and I'm okay."
+    print "I sleep all night and I work all day."
+
+repeat_lyrics()
