538b4a82ac24f4f14c2002cf7b8b09e324ae703f
Alexander Sobran <aisobran@ncsu.edu>
2015-08-27 07:42:49 -0400

    Exercise 3.1

---
+++ hw/code/2/think3.py
@@ -0,0 +1,10 @@
+# 3.1
+repeat_lyrics()
+def print_lyrics():
+    print "I'm a lumberjack, and I'm okay."
+    print "I sleep all night and I work all day."
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
