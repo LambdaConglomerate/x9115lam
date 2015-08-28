662376fb8a3946f6780f0eba375635e1f1f02da9
Matthew <meneal@gmail.com>
2015-08-28 18:25:20 -0400

    Working think4.py and images added to readme

--- hw/code/2/README.md
+++ hw/code/2/README.md
@@ -4,3 +4,16 @@
 3.2:
 The program runs as normal.
 ![3.2](./pics/3.2.png)
+
+###Exercise 4
+
+Running `think4.py` will open 6 windows with each of the flowers and each of the pies.  
+
+4.2:
+Images of the flowers are shown below:
+![4.2](./pics/think4_2.png)
+
+4.3: 
+Images of the pies are shown below:
+![4.3](./pics/think4_3.png)
+
--- hw/code/2/think4.py
+++ hw/code/2/think4.py
@@ -1,8 +1,7 @@
 from swampy.TurtleWorld import *
 import math
 
-# Setting stop=0 makes stop an optional argument
-# with default value 0
+# Stop is just the number of sides to draw
 def polygon(t, length, num_sides, stop=0):
   angle = 360.0 / num_sides
   if stop:
@@ -14,28 +13,19 @@ def polygon(t, length, num_sides, stop=0):
 def arc(t, radius, theta):
   circumference = 2 * math.pi * radius
   arc_length = (theta / 360.0) * circumference
-  # Could refactor per the book to define based
-  # on circumference, but not running slow enough
-  # to bother
+  # Could base on circumference, but not bothering
   num_sides = 100
   seg_length = circumference / num_sides
-  # Calculates the number of sides to draw
-  # to reach the correct arc_length
   stop = arc_length / seg_length
-  polygon(t, seg_length, num_sides, int(stop))
+  polygon(t, seg_length, num_sides, stop=int(stop))
 
 def circle(t, radius):
   arc(t, radius, 360)
-  # circumference = 2 * math.pi * radius
-  # num_sides = 100
-  # length = circumference / num_sides
-  # polygon(t, length, num_sides)
 
 def petal(t, radius, angle):
-  arc(t, radius, angle)
-  lt(t, 180 - angle)
-  arc(t, radius, angle)
-  lt(t, 180 - angle)
+  for i in range (2):
+    arc(t, radius, angle)
+    lt(t, 180 - angle)
 
 def flower(t, num_petals, petal_angle, radius):
   angle_increment = 360.0 / num_petals
@@ -56,59 +46,49 @@ def diag_angle(num_sides, length):
 
 def pie(t, length, num_sides):
   polygon(t, length, num_sides)
-
-  # diag = (.5 * length)/.59
   diagonal_angle = diag_angle(num_sides, length)
   diag = diag_length(num_sides, length, diagonal_angle)
   triangle_peak = 180 - (diagonal_angle + 90)
   lt(t, diagonal_angle)
   fd(t, diag)
-  for i in range (num_sides - 1):
-    lt(t, triangle_peak)
-    fd(t, diag)
-    lt(t, 180)
-    fd(t, diag)
+  if(num_sides % 2 != 0):
+    for i in range (num_sides - 1):
+      lt(t, triangle_peak)
+      fd(t, diag)
+      lt(t, 180)
+      fd(t, diag)
+  else:
+    for i in range (num_sides - 1):
+      lt(t, diagonal_angle * 2)
+      fd(t, diag)
+      lt(t, 180)
+      fd(t, diag)
 
-  wait_for_user()
-
-# def flower1():
-#   for i in range (8):
-#     arc(bob, 50, 90)
-#     lt(bob, 135)
-
-# def flower2():
-#   for i in range (20):
-#     arc(bob, 50, 90)
-#     lt(bob, 90)
-#     if(i % 2 == 0):
-#       lt(bob, 36)
-
-world = TurtleWorld()
-bob = Turtle()
-bob.delay = 0.1
-pie(bob, 50, 9)
-
-
-
+def clear_bob(delay):
+  world = TurtleWorld()
+  bob = Turtle()
+  bob.delay = delay
+  return bob
 
 def draw_flowers():
   # Flower1
-  world.clear()
-  bob = Turtle()
-  bob.delay = 0.01
-  flower(bob, 7, 40.0, 100.0)
-
+  flower(clear_bob(0.01), 7, 40.0, 100.0)
   # Flower2
-  world.clear()
-  bob = Turtle()
-  bob.delay = 0.01
-  flower(bob, 10, 90.0, 100.0)
-
+  flower(clear_bob(0.01), 10, 90.0, 100.0)
   # Flower3
-  world.clear()
+  flower(clear_bob(0.01), 20, 18, 250.0)
+
+def draw_pies():
+  for i in range (3):
+    pie(clear_bob(0.1), 50, (i + 5))
+  wait_for_user()
+
+def main():
   bob = Turtle()
-  bob.delay = 0.01
-  flower(bob, 20, 18, 250.0)
+  draw_flowers()
+  draw_pies()
+
+main()
 
 
 # arc(bob, 50, 180)
