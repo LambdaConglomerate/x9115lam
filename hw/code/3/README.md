167b4cc6713655c2fa09d993a91669425847c935
Alexander Sobran <aisobran@ncsu.edu>
2015-09-05 10:23:53 -0400

    10-8 finished, pic added to readme

---
+++ hw/code/3/README.md
@@ -0,0 +1,2 @@
+##Exercise 10.8 Birthday Paradox, Demonstrate has_duplicates and calcualte probability from sampling:
+![10.8](./pics/10-8.png)
--- hw/code/3/think10.py
+++ hw/code/3/think10.py
@@ -13,10 +13,16 @@ def has_duplicates(x):
 def populateBirthdays(number):
 	return map(lambda _: random.randrange(1, 365, 1), range(number))
 
-def generateBirthdayStats(iterations):
-	x = filter(lambda x: x == True , map(lambda _: has_duplicates(populateBirthdays(23)), range(iterations)) ).__len__() / iterations
+def generateBirthdayStats(students, iterations):
+	x = filter(lambda x: x == True , map(lambda _: has_duplicates(populateBirthdays(students)), range(iterations)) ).__len__() /float(iterations)
 	return x
 
 
-print generateBirthdayStats(100000)
+a = [1, 2, 3, 4]
+b = [2, 5, 6, 5]
+print a, " has duplicates?: ", has_duplicates(a)
+print b, " has duplicates?: ", has_duplicates(b)
+
+print "Probability of 23 students having the same birthday: " , generateBirthdayStats(23, 100000)
+
 
