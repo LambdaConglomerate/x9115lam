f084b7d1359191e7f1671d1ef8d869a3b0d1513e
Alexander Sobran <aisobran@ncsu.edu>
2015-09-24 17:05:15 -0400

    base runner working

--- hw/code/5/MaxWalkSat.py
+++ hw/code/5/MaxWalkSat.py
@@ -3,20 +3,25 @@ import random, math, sys
 # Calculates f1 and f2 and then
 # returns the values in a tuple
 # f1,f2
-def osyczka2(x1, x2, x3, x4, x5, x6):
-  f1 = -(25*(x1-2)**2 + (x2 - 2)**2 + (x3 - 1)**2 * (x4-4)**2 + (x5 - 1)**2)
-  f2 = x1**2 + x2**2 + x3**2 + x4**2 + x5**2 + x6**2
+def osyczka2(x):
+  f1 = -(25*(x[0]-2)**2 + (x[1] - 2)**2 + (x[2] - 1)**2 * (x[3]-4)**2 + (x[4] - 1)**2)
+  f2 = x[0]**2 + x[1]**2 + x[2]**2 + x[3]**2 + x[4]**2 + x[5]**2
   return (f1, f2)
 
 #osyczka2 constraints: checks all costraints, returns true is all constraints met
 #new mutation should be done if this returns false
-def constraints(x1, x2, x3, x4, x5, x6):
+def constraints12(x1, x2):
   if(x1 + x2 - 2 < 0): return False
   if(6 - x1 - x2 < 0): return False
   if(2 - x2 + x1 < 0): return False
-  if(4 - (x3 - 3)**2 - x4 < 0): return False
-  if((x5 - 3)**3 + x6 - 4 < 0): return False
+  return True
 
+def constraints34(x3, x4):
+  if(4 - (x3 - 3)**2 - x4 < 0): return False
+  return True
+
+def constraints56(x5, x6):
+  if((x5 - 3)**3 + x6 - 4 < 0): return False
   return True
 
 
@@ -71,6 +76,31 @@ x4bound = bounds(0, 6)
 x5bound = bounds(-GLOBAL_BOUND_MAX, 5)
 x6bound = bounds(0, 10)
 
+def generateValidValues():
+  x1temp = x1bound(random.random())
+  x2temp = x2bound(random.random())
+  x3temp = x3bound(random.random())
+  x4temp = x4bound(random.random())
+  x5temp = x5bound(random.random())
+  x6temp = x6bound(random.random())
+
+  while(not constraints12(x1temp, x2temp)):
+    x1temp = x1bound(random.random())
+    x2temp = x2bound(random.random())
+  
+
+  while(not constraints34(x3temp, x4temp)):
+    x3temp = x3bound(random.random())
+    x4temp = x4bound(random.random())
+  
+
+  while(not constraints56(x5temp, x6temp)):
+    x5temp = x5bound(random.random())
+    x6temp = x6bound(random.random())
+  
+
+  return(x1temp, x2temp, x3temp, x4temp, x5temp, x6temp)
+
 #TODO: adapt for oszyzcka2 constraints
 def base_runner():
   f1_obs = []
@@ -85,9 +115,9 @@ def base_runner():
   # x5 <= 5
   # 0 <= x4 <= 6
   # 0 <= x6 <= 10
-  for j in range(1000):
-    x = random.random()
-    y_tup = oszyzcka2(x)
+  for j in range(100):
+    x = generateValidValues()
+    y_tup = osyczka2(x)
 
     obs.append(y_tup)
 
@@ -111,95 +141,96 @@ def base_runner():
   # everything works as expected.  Check it out if you want.
   #
 
-  # norm_f1_obs = [norm_f1(f1) for f1,f2 in f1_obs]
-  # norm_f2_obs = [norm_f2(f2) for f1,f2 in f2_obs]
+  norm_f1_obs = [norm_f1(f1) for f1,f2 in f1_obs]
+  norm_f2_obs = [norm_f2(f2) for f1,f2 in f2_obs]
 
-  # for norm_ob in norm_f2_obs:
-  #   print norm_ob
+  for norm_ob in norm_f2_obs:
+    print norm_ob
 
-  # print '---------------------'
-  # print 'f1_max ' + str(f1_obs[-1][0])
-  # print 'f1_max normalized ' + str(norm_f1(f1_obs[-1][0]))
-  # print 'f1_min ' + str(f1_obs[0][0])
-  # print 'f1_min normalized ' + str(norm_f1(f1_obs[0][0]))
-  # print '---------------------'
-  # print 'f2_max ' + str(f2_obs[-1][1])
-  # print 'f2_max normalized ' + str(norm_f2(f2_obs[-1][1]))
-  # print 'f2_min ' + str(f2_obs[0][1])
-  # print 'f2_min normalized ' + str(norm_f2(f2_obs[0][1]))
+  print '---------------------'
+  print 'f1_max ' + str(f1_obs[-1][0])
+  print 'f1_max normalized ' + str(norm_f1(f1_obs[-1][0]))
+  print 'f1_min ' + str(f1_obs[0][0])
+  print 'f1_min normalized ' + str(norm_f1(f1_obs[0][0]))
+  print '---------------------'
+  print 'f2_max ' + str(f2_obs[-1][1])
+  print 'f2_max normalized ' + str(norm_f2(f2_obs[-1][1]))
+  print 'f2_min ' + str(f2_obs[0][1])
+  print 'f2_min normalized ' + str(norm_f2(f2_obs[0][1]))
 
   return (norm_f1, norm_f2)
 
-def neighbor(x, temp):
-  epsilon = temp * random.random()
-  add = bool(random.getrandbits(1))
-  if add:
-    x += epsilon
-  else:
-    x -= epsilon
+# def neighbor(x, temp):
+#   epsilon = temp * random.random()
+#   add = bool(random.getrandbits(1))
+#   if add:
+#     x += epsilon
+#   else:
+#     x -= epsilon
 
-  if x < 0.0:
-    x = x + 1.0
+#   if x < 0.0:
+#     x = x + 1.0
 
-  if x > 1.0:
-    x = x - 1.0
+#   if x > 1.0:
+#     x = x - 1.0
      
-  return x
+#   return x
 
-def say(x):
-  sys.stdout.write(str(x)); sys.stdout.flush()
+# def say(x):
+#   sys.stdout.write(str(x)); sys.stdout.flush()
 
-def prob(old, new, k):
-  x = math.exp(((new - old) / k))
-  return x
+# def prob(old, new, k):
+#   x = math.exp(((new - old) / k))
+#   return x
 
-global kmax
-global emax
-kmax = 10000.0
-emax = (2)**0.5
+# global kmax
+# global emax
+# kmax = 10000.0
+# emax = (2)**0.5
 
-def sim_anneal(energy):
-  s0 = 0.5
-  s = s0
-  e = energy(s)
-  print "Initial energy", e
-  sb = s
-  eb = e
-  k = 1.0
+# def sim_anneal(energy):
+#   s0 = 0.5
+#   s = s0
+#   e = energy(s)
+#   print "Initial energy", e
+#   sb = s
+#   eb = e
+#   k = 1.0
   
-  say('(K:' + str(k) + ", SB:({0:.3f}) ".format(sb) + '\t')
+#   say('(K:' + str(k) + ", SB:({0:.3f}) ".format(sb) + '\t')
 
-  while k < kmax and e < emax:
-    sn = neighbor(s, (kmax - k)/kmax)
-    en = energy(sn)
+#   while k < kmax and e < emax:
+#     sn = neighbor(s, (kmax - k)/kmax)
+#     en = energy(sn)
 
-    # Is this a best overall?
-    if en > eb:
-      sb = sn
-      eb = en
-      say("!")
+#     # Is this a best overall?
+#     if en > eb:
+#       sb = sn
+#       eb = en
+#       say("!")
 
-    # Is this better than where we were last?
-    if en > e:
-      s = sn
-      e = en
-      say("+")
+#     # Is this better than where we were last?
+#     if en > e:
+#       s = sn
+#       e = en
+#       say("+")
 
-    elif prob(e, en, k / kmax) < random.random():
-      s = sn
-      e = en
-      say("?")
+#     elif prob(e, en, k / kmax) < random.random():
+#       s = sn
+#       e = en
+#       say("?")
 
-    say(".")
-    k += 1.00
+#     say(".")
+#     k += 1.00
 
-    if k % 50 == 0:
-      say("\n" + '(K:' + str(k) + ", SB:({0:.3f}) ".format(sb) + '\t')
+#     if k % 50 == 0:
+#       say("\n" + '(K:' + str(k) + ", SB:({0:.3f}) ".format(sb) + '\t')
 
-  print '\n \nbest solution ' + str(sb)
-  print 'energy of best solution ' + str(energy(sb))
+#   print '\n \nbest solution ' + str(sb)
+#   print 'energy of best solution ' + str(energy(sb))
 
 if __name__ == "__main__":
   norm_tup = base_runner()
   e = energy(norm_tup[0], norm_tup[1])
-  sim_anneal(e)
+  #sim_anneal(e)
+
