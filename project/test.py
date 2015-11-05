10f892f96f4c3faa83fe3f2cf28da7108638c2f9
Joseph Sankar <jesankar@ncsu.edu>
2015-11-05 13:46:28 -0500

    Began working on PSO

--- project/test.py
+++ project/test.py
@@ -1,8 +1,9 @@
 from Models import *
 from sa import *
 from mws import *
+from pso import *
 
-for m in [Schaffer, Osyczka2, Kursawe]:
-    for o in [sa, mws]:
+for m in [Osyczka2]:
+    for o in [pso]:
         #Absolutely do not pass integers for retries or changes
         o(m, 50.0, 500.0)
