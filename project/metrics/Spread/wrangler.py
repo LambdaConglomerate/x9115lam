abbd9f410528cd7a9f2b73d9fe2d9573171615b2
Matthew <meneal@gmail.com>
2015-11-27 16:42:06 -0600

    added all True frontiers including DTLZ

--- project/metrics/Spread/wrangler.py
+++ project/metrics/Spread/wrangler.py
@@ -1,15 +1,21 @@
-fn = "./True_PF/Water.txt"
-f = open(fn, "r")
-ln = f.readline()
-fin = ''
-while ln:
-  print ln
-  ln = ln.strip()
-  arr = ln.split()
-  ln = " ".join(arr)
-  fin += ln + '\n'
-  ln = f.readline()
-f.close()
+path = "./True_PF/ZDT"
 
-f = open(fn, "w")
-f.write(fin)
+for i in xrange(1,6):
+  if i == 5:
+    continue
+  print i
+  fn = path + str(i) +".txt"
+  f = open(fn, "r")
+  ln = f.readline()
+  fin = ''
+  while ln:
+    print ln
+    ln = ln.strip()
+    arr = ln.split()
+    ln = " ".join(arr)
+    fin += ln + '\n'
+    ln = f.readline()
+  f.close()
+
+  f = open(fn, "w")
+  f.write(fin)
