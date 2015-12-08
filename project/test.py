e93d3ed292bc0d9a6dfd261f272c33874e4cf90d
Alexander Sobran <aisobran@ncsu.edu>
2015-12-08 10:51:48 -0500

    remove particle tracker

--- project/test.py
+++ project/test.py
@@ -12,10 +12,10 @@ from PSOv2 import *
 Two_D_List = [Osyczka2, Fonseca, ZDT1, ZDT2, ZDT3, ZDT4, Tanaka, Srinivas]
 Three_D_List = [DTLZ1, DTLZ2, DTLZ3, DTLZ4, DTLZ5, DTLZ6, DTLZ7, Viennet2, Viennet3]
 
-num_retries = 1.0
-num_changes = 5000.0
+num_retries = 30.0
+num_changes = 100.0
 
-for m in Three_D_List:
+for m in Two_D_List:
     for o in [PSOProbs]:
         #Absolutely do not pass integers for retries or changes
         if(len(sys.argv) > 1):
